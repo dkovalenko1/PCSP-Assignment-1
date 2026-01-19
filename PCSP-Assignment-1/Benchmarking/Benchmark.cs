2a6736e9664bb1c6378ee68008d556dc5c200652
dkovalenko1 <dkovalenko1@kse.org.ua>
2026-01-19 13:12:12 +0200

    feat: median implementation and manual GC usage between runs

--- PCSP-Assignment-1/Benchmarking/Benchmark.cs
+++ PCSP-Assignment-1/Benchmarking/Benchmark.cs
@@ -5,14 +5,14 @@ namespace PCSP_Assignment_1.Benchmarking;
 
 public class Benchmark
 {
-    private readonly int[] _sizes = [50, 100, 500, 1000, 10000, 20000, 30000];
+    private readonly int[] _sizes = [50, 100, 500, 1000, 5000, 10000, 15000, 20000, 25000, 30000];
     private readonly int[] _threads;
     private readonly List<string> _csvLines = [];
 
     public Benchmark()
     {
         var cores = Environment.ProcessorCount;
-        _threads = [cores / 6, cores / 4, cores / 2, 8, 10, cores, cores * 2,  cores * 4,   cores * 8, cores * 16];
+        _threads = [cores / 6, cores / 4, cores / 2, 8, 10, cores, 16, 20, cores * 2,  cores * 4,   cores * 8, cores * 16, cores * 20];
     }
 
     public void Run()
@@ -25,7 +25,8 @@ public class Benchmark
         foreach (var size in _sizes)
         {
             var (genTime, sortTime) = MeasureSequential(size);
-            _csvLines.Add($"{size},1,{genTime:F3},{sortTime:F3},{genTime + sortTime:F3}");
+            _csvLines.Add(string.Format(CultureInfo.InvariantCulture,
+                "{0},1,{1:F3},{2:F3},{3:F3}", size, genTime, sortTime, genTime + sortTime));
         }
 
         foreach (var size in _sizes)
@@ -33,7 +34,8 @@ public class Benchmark
             foreach (var threads in _threads)
             {
                 var (genTime, sortTime) = MeasureParallel(size, threads);
-                _csvLines.Add($"{size},{threads},{genTime:F3},{sortTime:F3},{genTime + sortTime:F3}");
+                _csvLines.Add(string.Format(CultureInfo.InvariantCulture,
+                    "{0},{1},{2:F3},{3:F3},{4:F3}", size, threads, genTime, sortTime, genTime + sortTime));
             }
         }
 
@@ -44,39 +46,68 @@ public class Benchmark
 
     private static (double genTime, double sortTime) MeasureSequential(int size)
     {
-        var warmup = new Matrix(100);
-        warmup.GenerateRandomSeq();
-        warmup.PlaceMaxOnDiagonalSeq();
+        for (var w = 0; w < 3; w++)
+        {
+            var warmup = new Matrix(size);
+            warmup.GenerateRandomSeq();
+            warmup.PlaceMaxOnDiagonalSeq();
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
         var genTimes = new List<double>();
         var sortTimes = new List<double>();
 
         for (var i = 0; i < 5; i++)
         {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
             var m = new Matrix(size);
             genTimes.Add(ActionTimer.Measure(m.GenerateRandomSeq).TotalMilliseconds);
             sortTimes.Add(ActionTimer.Measure(m.PlaceMaxOnDiagonalSeq).TotalMilliseconds);
         }
 
-        return (genTimes.Average(), sortTimes.Average());
+        return (Median(genTimes), Median(sortTimes));
     }
 
     private static (double genTime, double sortTime) MeasureParallel(int size, int threads)
     {
-        var warmup = new Matrix(100);
-        warmup.GenerateRandomParallel(threads);
-        warmup.PlaceMaxOnDiagonalParallel(threads);
+        for (var w = 0; w < 3; w++)
+        {
+            var warmup = new Matrix(size);
+            warmup.GenerateRandomParallel(threads);
+            warmup.PlaceMaxOnDiagonalParallel(threads);
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
         var genTimes = new List<double>();
         var sortTimes = new List<double>();
 
         for (var i = 0; i < 5; i++)
         {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
             var m = new Matrix(size);
             genTimes.Add(ActionTimer.Measure(() => m.GenerateRandomParallel(threads)).TotalMilliseconds);
             sortTimes.Add(ActionTimer.Measure(() => m.PlaceMaxOnDiagonalParallel(threads)).TotalMilliseconds);
         }
 
-        return (genTimes.Average(), sortTimes.Average());
+        return (Median(genTimes), Median(sortTimes));
+    }
+
+    private static double Median(List<double> values)
+    {
+        var sorted = values.OrderBy(x => x).ToList();
+        var count = sorted.Count;
+        if (count % 2 == 0)
+            return (sorted[count / 2 - 1] + sorted[count / 2]) / 2.0;
+        return sorted[count / 2];
     }
 }
